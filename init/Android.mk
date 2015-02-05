64f042b0e5381c9cc9a535694b0d3be42ebee433
Readone !! ErcOne <android.armv7a@gmail.com>
2015-02-05 21:54:42 +0700

    Update Android.mk

--- init/Android.mk
+++ init/Android.mk
@@ -32,20 +32,6 @@ endif
 # Enable ueventd logging
 #LOCAL_CFLAGS += -DLOG_UEVENTS=1
 
-SYSTEM_CORE_INIT_DEFINES := BOARD_CHARGING_MODE_BOOTING_LPM \
-    BOARD_CHARGING_CMDLINE_NAME \
-    BOARD_CHARGING_CMDLINE_VALUE
-
-$(foreach system_core_init_define,$(SYSTEM_CORE_INIT_DEFINES), \
-  $(if $($(system_core_init_define)), \
-    $(eval LOCAL_CFLAGS += -D$(system_core_init_define)=\"$($(system_core_init_define))\") \
-  ) \
-)
-
-ifneq ($(TARGET_NR_SVC_SUPP_GIDS),)
-LOCAL_CFLAGS += -DNR_SVC_SUPP_GIDS=$(TARGET_NR_SVC_SUPP_GIDS)
-endif
-
 LOCAL_MODULE:= init
 
 LOCAL_FORCE_STATIC_EXECUTABLE := true
@@ -60,19 +46,12 @@ LOCAL_STATIC_LIBRARIES := \
 	libc \
 	libselinux \
 	libmincrypt \
-	libext4_utils_static \
-	libext2_blkid \
-	libext2_uuid
+	libext4_utils_static
 
 LOCAL_ADDITIONAL_DEPENDENCIES += $(LOCAL_PATH)/Android.mk
-ifneq ($(strip $(TARGET_PLATFORM_DEVICE_BASE)),)
-LOCAL_CFLAGS += -D_PLATFORM_BASE="\"$(TARGET_PLATFORM_DEVICE_BASE)\""
-endif
+
 ifneq ($(strip $(TARGET_INIT_VENDOR_LIB)),)
 LOCAL_WHOLE_STATIC_LIBRARIES += $(TARGET_INIT_VENDOR_LIB)
-endif
-ifneq ($(strip $(TARGET_PROP_PATH_FACTORY)),)
-LOCAL_CFLAGS += -DOVERRIDE_PROP_PATH_FACTORY=\"$(TARGET_PROP_PATH_FACTORY)\"
 endif
 
 include $(BUILD_EXECUTABLE)
